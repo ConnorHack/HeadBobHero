dd812ed81f0eda477a7f95f2e30611c33ac5f174
Connor <connorhack10@gmail.com>
2015-10-01 23:41:26 -0400

    Added the activity to record head-bobs
    - Initializes the Gravity Sensor
    - Modified the AndroidManifest for SDK versions

--- app/src/main/java/com/example/ben/headbobhero/MainActivity.java
+++ app/src/main/java/com/example/ben/headbobhero/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ben.headbobhero;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent recordingIntent = new Intent(this, RecordingActivity.class);
+        recordingIntent.setAction("record_headbobs");
+        startActivity(recordingIntent);
     }
 
     @Override
