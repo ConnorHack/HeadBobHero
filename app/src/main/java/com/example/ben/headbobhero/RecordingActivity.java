c774503aed22aab23c8925e34c424276537ce998
Connor <connorhack10@gmail.com>
2015-10-06 14:24:34 -0400

    Correctly detecting head-bobs at the highest level

--- app/src/main/java/com/example/ben/headbobhero/RecordingActivity.java
+++ app/src/main/java/com/example/ben/headbobhero/RecordingActivity.java
@@ -11,24 +11,21 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by Connor on 10/1/2015.
- *
+ * <p/>
  * <h1>Activity to record head-bobs overlaying a currently playing song</h1>
- *
+ * <p/>
  * This activity will house the recognition of specific head-bobs & storing the
  * head-bobs while relating them to the playing song.
  *
  * @author Connor
- * @since 10/1/2015
  * @version 1.0
+ * @since 10/1/2015
  */
 public class RecordingActivity extends Activity implements SensorEventListener {
 
-    private static final String DEBUG = "RecordingActivity" ;
+    private static final String DEBUG = "RecordingActivity";
 
     /* Constants created for ease of use within code
      * DELAY_NORMAL:    7.14 records per second
@@ -42,14 +39,15 @@ public class RecordingActivity extends Activity implements SensorEventListener {
     public static final int DELAY_FASTEST = SensorManager.SENSOR_DELAY_FASTEST;
 
     // Rate at which to collect values from sensors
-    private static int RATE = DELAY_GAME ;
+    private static int RATE = DELAY_FASTEST;
 
     // Globals needed to collect values from device's sensors
-    private SensorManager mSensorManager ;
-    private Sensor mSensorGravity ;
-    private float mGravity[] ;
+    private SensorManager mSensorManager;
+    private Sensor mSensorGravity;
+    private float mGravity[];
 
-    private boolean mIsRecording ;
+    private boolean mIsRecording;
+    private HeadBobDirection mCurrentBob;
 
     private long timeStart;
 
@@ -62,6 +60,8 @@ public class RecordingActivity extends Activity implements SensorEventListener {
     }
 
     private final BobRecording bobRecordingState = new BobRecording();
+
+    private boolean mRegisteringBob;
 
     /////////////////////////////////////////////////////
     // Below methods are for the extension of Activity //
@@ -118,18 +118,18 @@ public class RecordingActivity extends Activity implements SensorEventListener {
 
         // Check first to make sure we are supposed to be collecting results
         if (mIsRecording) {
-            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            if (event.sensor.getType() == Sensor.TYPE_GRAVITY) {
                 mGravity = lowPassFilter(event.values, mGravity);
             }
 
             if (mGravity != null) {
-                 //DEBUG STATEMENT: Testing certain head-bob thresholds
-                 //System.out.println(mGravity[0] + " " + mGravity[1] + " " + mGravity[2]);
-                long now = System.currentTimeMillis();
+                //DEBUG STATEMENT: Testing certain head-bob thresholds
+                //System.out.println(mGravity[0] + " " + mGravity[1] + " " + mGravity[2]);
+/*                long now = System.currentTimeMillis();
                 if(now < lastBobTime + 500) {
                     return;
                 }
-                lastBobTime = now;
+                lastBobTime = now;*/
 
                 long offset = bobRecordingState.currentOffset;
 
@@ -137,17 +137,50 @@ public class RecordingActivity extends Activity implements SensorEventListener {
                 //    return;
                 //}
                 //lastOffset = offset;
-
-                 if (mGravity[0] < -2.7) {
-                      GameBoard.headBobs.add(new HeadBob(offset, HeadBobDirection.RIGHT));
-                     System.out.println(offset + " RIGHT");
-                 } else if (mGravity[0] > 2.7) {
-                     GameBoard.headBobs.add(new HeadBob(offset, HeadBobDirection.LEFT));
-                     System.out.println(offset + " LEFT");
-                 } else if (mGravity[1] < 9.5) {
-                     GameBoard.headBobs.add(new HeadBob(offset, HeadBobDirection.DOWN));
-                     System.out.println(offset + " DOWN");
-                 }
+                if (!mRegisteringBob) {
+                    if (mGravity[0] < -2.0) {
+                        GameBoard.headBobs.add(new HeadBob(offset, HeadBobDirection.RIGHT));
+                        System.out.println(offset + " RIGHT");
+                        mRegisteringBob = true;
+                        mCurrentBob = HeadBobDirection.RIGHT;
+                    } else if (mGravity[0] > 0.5) {
+                        GameBoard.headBobs.add(new HeadBob(offset, HeadBobDirection.LEFT));
+                        System.out.println(offset + " LEFT");
+                        mRegisteringBob = true;
+                        mCurrentBob = HeadBobDirection.LEFT;
+                    } else if (mGravity[1] < 9.3) {
+                        GameBoard.headBobs.add(new HeadBob(offset, HeadBobDirection.DOWN));
+                        System.out.println(offset + " DOWN");
+                        mRegisteringBob = true;
+                        mCurrentBob = HeadBobDirection.DOWN;
+                    }
+                } else {
+                    switch (mCurrentBob) {
+                        case RIGHT:
+                            if (mGravity[0] > -2.0) {
+                                mRegisteringBob = false;
+                                mCurrentBob = null;
+                                System.out.println(offset + " REST from RIGHT");
+                            }
+                            break;
+                        case LEFT:
+                            if (mGravity[0] < 0.5) {
+                                mRegisteringBob = false;
+                                mCurrentBob = null;
+                                System.out.println(offset + " REST from LEFT");
+                            }
+                            break;
+                        case DOWN:
+                            if (mGravity[1] > 9.3) {
+                                mRegisteringBob = false;
+                                mCurrentBob = null;
+                                System.out.println(offset + " REST from DOWN");
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
         }
     }
@@ -163,7 +196,7 @@ public class RecordingActivity extends Activity implements SensorEventListener {
     public void initGlobals() {
 
         // Initialize the sensor manager, gravity sensor, and gravity vector
-        mGravity = new float[3] ;
+        mGravity = new float[3];
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mSensorGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         timeStart = System.currentTimeMillis();
@@ -171,37 +204,39 @@ public class RecordingActivity extends Activity implements SensorEventListener {
         // Check to ensure the gravity sensor is functional on the device
         if (mSensorGravity == null) {
             Log.e(DEBUG, "Device does not possess a gravity sensor. "
-                        + "This application will not function properly.");
+                    + "This application will not function properly.");
         }
 
         // Initially do not begin collecting results
-        mIsRecording = true ;
+        mIsRecording = true;
+
+        // Initially do not register a head-bob
+        mRegisteringBob = false;
     }
 
     /**
-     * @author Connor Hack
-     * <h2>Function to apply a Low Pass Filter on a data-set</h2>
-     *
-     * Allows low-frequency signals to pass and will reduce the amplitude of
-     * high frequency signals.
-     *
-     * @param input The data-set to modify
+     * @param input  The data-set to modify
      * @param output The data-set to store the filtered values
      * @return Filtered Values
+     * @author Connor Hack
+     * <h2>Function to apply a Low Pass Filter on a data-set</h2>
+     * <p/>
+     * Allows low-frequency signals to pass and will reduce the amplitude of
+     * high frequency signals.
      */
     public float[] lowPassFilter(float[] input, float[] output) {
 
         // Return if the size of the two lists do not match
         if (output.length != input.length) {
-            return input ;
+            return input;
         }
 
         // Appply a low pass filter to the input
-        for (int i=0; i < input.length; i++) {
+        for (int i = 0; i < input.length; i++) {
             output[i] = output[i] + 0.1f * (input[i] - output[i]);
         }
 
-        return output ;
+        return output;
     }
 
     private static final int FRAME_RATE = 20; //50 frames per second
@@ -215,7 +250,7 @@ public class RecordingActivity extends Activity implements SensorEventListener {
             frame.removeCallbacks(frameUpdate);
             //make any updates to on screen objects here
             //then invoke the on draw by invalidating the canvas
-            bobRecordingState.currentOffset -=4;
+            bobRecordingState.currentOffset += 4;
             frame.postDelayed(frameUpdate, FRAME_RATE);
         }
     };
