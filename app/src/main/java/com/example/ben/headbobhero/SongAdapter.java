349e54d2e4a3168b507af2a8bf10d4aa8a34c8cd
Carlos Castro <cjc4372@rit.edu>
2015-10-19 14:30:35 -0400

    Added SongAdapter and modified MainActivity to use the new Adapter

---
+++ app/src/main/java/com/example/ben/headbobhero/SongAdapter.java
@@ -0,0 +1,49 @@
+package com.example.ben.headbobhero;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Carlos on 10/18/2015.
+ */
+public class SongAdapter extends ArrayAdapter<RegisteredSong> {
+
+    public SongAdapter(Context context, int textViewResourceId) {
+        super(context, textViewResourceId);
+    }
+
+    public SongAdapter(Context context, int resource, List<RegisteredSong> songs) {
+        super(context, resource, songs);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = convertView;
+
+        if (v == null) {
+            LayoutInflater vi;
+            vi = LayoutInflater.from(getContext());
+            v = vi.inflate(R.layout.row_layout, null);
+        }
+
+        RegisteredSong p = getItem(position);
+
+        if (p != null) {
+            TextView tt1 = (TextView) v.findViewById(R.id.song);
+
+            if (tt1 != null) {
+                tt1.setText(p.getSongName());
+            }
+        }
+
+        return v;
+    }
+
+}
